a096e7c73e261204c17d920759a584d8ee1b4eb5
Alireza-Kolaei <kolaei.alireza79@gmail.com>
2023-06-07 14:53:25 +0330

    mongo repo finished

--- src/repository/IRepository.ts
+++ src/repository/IRepository.ts
@@ -1,12 +1,24 @@
 export default interface IRepository<T> {
-  findByID(ID: string, relations?: string[]): Promise<T | null>;
-  findOneByParams(params: Partial<T>, relations?: string[]): Promise<T | null>;
-  findManyByParams(params: Partial<T>, relations?: string[]): Promise<T[]>;
+  findByID(ID: string, projection?: string, relations?: string[]): Promise<T | null>;
+  findOneByParams(params: Partial<T>, projection?: string, relations?: string[]): Promise<T | null>;
+
+  findMany(
+    params: Partial<T> | object,
+    relations?: string[],
+    projection?: string,
+    sort?: string,
+    pagination?: { page: number; limit: number } | undefined
+  ): Promise<T[]>;
+
   create(params: Partial<T>): Promise<T>;
+
   updateOne(where: Partial<T>, updateData: Partial<T>): Promise<T | null>;
-  updateMany(where: Partial<T>, updateData: Partial<T>): Promise<T[] | null>;
-  deleteOne(ID: string): Promise<boolean>;
-  deleteMany(where: any): Promise<boolean>;
+  updateMany(where: Partial<T>, updateData: Partial<T>): Promise<object>;
+
+  deleteOneByID(ID: string): Promise<T | null>;
+  deleteOne(params: Partial<T>): Promise<T | null>;
+
+  deleteMany(where: Partial<T>): Promise<object>;
 
   // count(params:any):Promise<number>
 }
--- src/repository/mongo.repository.ts
+++ src/repository/mongo.repository.ts
@@ -1,4 +1,4 @@
-import { Model, model } from 'mongoose';
+import { Model, model, Query } from 'mongoose';
 import APIFeatures from '../utils/ApiFeatures';
 import IRepository from './IRepository';
 
@@ -8,36 +8,54 @@ export default class MongoRepository<T> implements IRepository<T> {
     this.model = model;
   }
 
-  findByID(ID: string, relations?: string[] | undefined): Promise<T | null> {
+  findByID(ID: string, projection?: string, relations?: string[] | undefined): Promise<T | null> {
     const query = this.model.findById(ID);
     if (relations && relations.length > 0) {
       relations.forEach((relation: string) => {
         query.populate(relation);
       });
     }
+    if (projection && projection.length > 0) {
+      query.select(projection);
+    }
     return query.exec();
   }
 
-  findOneByParams(params: Partial<T>, relations?: string[] | undefined): Promise<T | null> {
-    const query = this.model.findOne({params});
+  findOneByParams(params: Partial<T>, projection?: string, relations?: string[] | undefined): Promise<T | null> {
+    const query = this.model.findOne(params);
     if (relations && relations.length > 0) {
       relations.forEach((relation: string) => {
         query.populate(relation);
       });
     }
+    if (projection && projection.length > 0) {
+      query.select(projection);
+    }
     return query.exec();
   }
 
-  findManyByParams(params: Partial<T>, relations?: string[] | undefined): Promise<T[]> {
-    const query = this.model.find({ params });
+  findMany(
+    params: Partial<T> | object,
+    relations?: string[],
+    projection?: string,
+    sort?: string,
+    pagination?: { page: number; limit: number } | undefined
+  ): Promise<T[]> {
+    let query = this.model.find(params).select(projection);
+    if (pagination) {
+      const page = pagination.page || 1;
+      const limit = pagination.limit || 100;
+      const skip = (page - 1) * limit;
+      query = query.skip(skip).limit(limit);
+    }
     if (relations && relations.length > 0) {
       relations.forEach((relation: string) => {
         query.populate(relation);
       });
     }
-    return query.exec();
-  }
 
+    return query.sort(sort).exec();
+  }
 
   async create(params: Partial<T>): Promise<T> {
     const doc = this.model.create({ ...params });
@@ -45,50 +63,26 @@ export default class MongoRepository<T> implements IRepository<T> {
   }
 
   updateOne(where: Partial<T>, updateData: Partial<T>): Promise<T | null> {
-    const doc = this.model.findOneAndUpdate(where, updateData ,{new:true , runValidators : true});
-    return doc.exec()
+    const doc = this.model.findOneAndUpdate(where, updateData, { new: true, runValidators: true });
+    return doc.exec();
   }
 
-  updateMany(where: Partial<T>, updateData: Partial<T>): Promise<T[] | null> {
-    const docs = this.model.updateMany( where, updateData)
-    return docs.exec()
-  }
-  
-  deleteOne(ID: string): Promise<boolean> {
-    throw new Error('Method not implemented.');
-  }
-  deleteMany(where: any): Promise<boolean> {
-    throw new Error('Method not implemented.');
+  updateMany(where: Partial<T>, updateData: Partial<T>): Promise<object> {
+    const docs = this.model.updateMany(where, updateData);
+    return docs.exec();
   }
 
-  // public async findOne(params: any, popOptions?: any): Promise<any> {
-  //   let query = this.model.findOne(params);
-  //   if (popOptions) query = query.populate(popOptions);
-  //   return query;
-  // }
-  // public async findByID(ID: string, popOptions?: any): Promise<any> {
-  //   let query = this.model.findById(ID);
-  //   if (popOptions) query = query.populate(popOptions);
-  //   return query;
-  // }
-  // public async findMany(reqQuery: any, tourId?: string): Promise<any> {
-  //   let filter = {};
-  //   if (tourId) filter = { tour: tourId };
+  deleteOneByID(ID: string): Promise<T | null> {
+    const doc = this.model.findByIdAndDelete(ID);
+    return doc.exec();
+  }
 
-  //   const features = new APIFeatures(this.model.find(filter), reqQuery).filter().sort().limitFields().paginate();
-  //   return await features.query;
-  // }
-  // public async createOne(params: any) {
-  //   const doc = await this.model.create(params);
-  //   return doc;
-  // }
-  // public async updateOne(ID: string, params: any) {
-  //   return this.model.findByIdAndUpdate(ID, params, {
-  //     new: true,
-  //     runValidators: true,
-  //   });
-  // }
-  // public async deleteOne(ID: string) {
-  //   return this.model.findByIdAndDelete(ID);
-  // }
+  deleteOne(params: Partial<T>): Promise<T | null> {
+    const doc = this.model.findOneAndDelete(params);
+    return doc.exec();
+  }
+
+  deleteMany(where: Partial<T>): Promise<object> {
+    return this.model.deleteMany(where).exec();
+  }
 }
